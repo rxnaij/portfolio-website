509a27ef44dd25971f735d0141e11b6f17760af9
rxnaij <richardblue37@gmail.com>
2021-03-23 15:34:15 -0400

    fix bug in which String.prototype.replaceAll() is unsupported

--- src/templates/slugUtil.ts
+++ src/templates/slugUtil.ts
@@ -1,7 +1,7 @@
 export const generateSlugFromTitle = (title: string) => {
     return encodeURIComponent(
         title.toLowerCase()
-            .replaceAll(' ', '-')
-            .replaceAll(/\W-/g, '')
+            .replace(' ', '-')
+            .replace(/\W-/g, '')
         )
 }
