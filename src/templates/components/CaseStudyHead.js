e335d58c9289e1ed616d892dfc91cb131ac02b30
rxnaij <richardblue37@gmail.com>
2021-03-29 02:45:30 -0400

    add container to head

--- src/templates/components/CaseStudyHead.js
+++ src/templates/components/CaseStudyHead.js
@@ -18,41 +18,52 @@ const CaseStudyHead = ({
     projectLink 
 }) => (
     <section className={head}>
-        <div className={titleClass}>
-            <nav><Link to="/work"><small>&larr; Back to portfolio</small></Link></nav>
-            <h1>{title}</h1>  
-            <p className="lead">{description}</p>
-            {
-                projectLink && 
-                <p className="lead">
-                    <strong>Try it out:</strong> <a href={projectLink}>{projectLink}</a>
-                </p>
-            }
-            <ul className={info}>
-                <li className="mb-2">
-                    <strong>Date</strong>
-                    {/* <hr /> */}
-                    <div>{  startDate + ( endDate ? ` – ${endDate}` : `` ) }</div>
-                </li>
-                <li className="mb-2">
-                    <strong>Project type</strong>
-                    {/* <hr /> */}
-                    <div>{ projectType }</div>
-                </li>
-                <li className="mb-2">
-                    <strong>Role</strong>
-                    {/* <hr /> */}
-                    <div>{ role }</div>
-                </li>
-                {/* { projectLink && <li className="mb-2"><strong>View app website</strong><br /><a href={projectLink}>{projectLink}</a> </li> } */}
-            </ul>
-        </div>
-        <GatsbyImage 
-            image={coverPhoto.gatsbyImageData} 
-            alt={coverPhoto.title} 
-            className={img}
-            loading="eager" 
-        />
+        <Container>
+            <Row>
+                <Col lg={6}>
+                    <div className={titleClass}>
+                        <nav><Link to="/work"><small>&larr; Back to portfolio</small></Link></nav>
+                        <h1>{title}</h1>  
+                        <p className="lead">{description}</p>
+                        {
+                            projectLink && 
+                            <p className="lead">
+                                <strong>Try it out:</strong> <a href={projectLink}>{projectLink}</a>
+                            </p>
+                        }
+                        <ul className={info}>
+                            <li className="mb-2">
+                                <strong>Date</strong>
+                                {/* <hr /> */}
+                                <div>{  startDate + ( endDate ? ` – ${endDate}` : `` ) }</div>
+                            </li>
+                            <li className="mb-2">
+                                <strong>Project type</strong>
+                                {/* <hr /> */}
+                                <div>{ projectType }</div>
+                            </li>
+                            <li className="mb-2">
+                                <strong>Role</strong>
+                                {/* <hr /> */}
+                                <div>{ role }</div>
+                            </li>
+                            {/* { projectLink && <li className="mb-2"><strong>View app website</strong><br /><a href={projectLink}>{projectLink}</a> </li> } */}
+                        </ul>
+                    </div>
+                </Col>
+                <Col lg={6} className="bg-light no-gutters">
+                    <GatsbyImage 
+                        image={coverPhoto.gatsbyImageData} 
+                        alt={coverPhoto.title} 
+                        className={img}
+                        loading="eager" 
+                    />
+                </Col>
+            </Row>
+                
+            
+        </Container>
+        
     </section>
 )
 
