51eb1d66d37c0b5a7306d8016ef2c9880c92951b
rxnaij <richardblue37@gmail.com>
2021-05-09 01:01:06 -0400

    fix bug where clicking TOC links collapsed the TOC

--- src/templates/components/TableOfContents.tsx
+++ src/templates/components/TableOfContents.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
+import classNames from 'classnames'
 
 import { generateSlugFromTitle } from '../slugUtil'
 import { ChevronCompactUp, ChevronCompactDown } from 'react-bootstrap-icons'
 
-import { tocClass, iconClass, activeLinkClass, labelClass } from './TableOfContents.module.scss'
+import { tocClass, iconClass, chevronIsOpen, activeLinkClass, labelClass } from './TableOfContents.module.scss'
 import { exampleData } from '../exampleData'
 import { UnorderedListNode, UnorderedListNodeProps } from '../../components/lists/UnorderedListNode'
 import { HeadingNode } from '../../components/lists/HeadingUtils'
@@ -12,16 +13,33 @@ interface TableOfContentsProps extends UnorderedListNodeProps {
     headings: Array<HeadingNode>
 }
 
+const getIDs = (headings: Array<HeadingNode>) => {
+    return headings.reduce((accumulator, heading) => {
+        if (heading.slug) {
+            accumulator.push(heading.slug)
+        }
+        return accumulator
+    })
+}
+
+const useActiveId = () => {
+
+}
+
 const TableOfContents = ({ headings }: TableOfContentsProps) => {
     const [isOpen, setOpen] = useState(true)
     return (
-        <nav className={tocClass} onClick={() => setOpen(!isOpen)}>
-            <div className={labelClass}>
+        <nav className={tocClass}>
+            <div className={labelClass} onClick={() => setOpen(!isOpen)}>
                 <div>Table of contents</div>
                 <div>
+                    {/* <ChevronCompactDown 
+                        className={classNames(iconClass, isOpen && chevronIsOpen)} 
+                        aria-label={isOpen ? "Close section navigation" : "Open section navigation"} 
+                    /> */}
                     { isOpen 
-                        ? <ChevronCompactUp className={iconClass} aria-label="Close section navigation" /> 
-                        : <ChevronCompactDown className={iconClass} aria-label="Open section navigation" /> 
+                        ? <><ChevronCompactUp className={iconClass} aria-label="Close section navigation" /> </>
+                        : <><ChevronCompactDown className={iconClass} aria-label="Open section navigation" /> </>
                     }
                 </div>
             </div>
--- src/templates/slugUtil.ts
+++ src/templates/slugUtil.ts
@@ -1,7 +1,12 @@
+/**
+ * Returns a valid slug to use in anchor links for page sections. 
+ * @param title the title of the page section, to be converted into a slug
+ * @returns the page section title as a valid slug, removed of illegal characters
+ */
 export const generateSlugFromTitle = (title: string) => {
     return (
         title.toLowerCase()
-            .replace(/' '/g, '-')
-            .replace(/\W-/g, '')
+            .replace(/[^a-z0-9\s]/g, '')
+            .replace(/\s/g, '-')
         )
 }
