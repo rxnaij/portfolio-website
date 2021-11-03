b2f5a2491e0e74788b460623886f2f994edbeff4
rxnaij <richardblue37@gmail.com>
2021-11-03 01:04:07 -0400

    add container to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,12 +4,18 @@ import { Link }  from 'gatsby'
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 
+import Container from 'react-boostrap/Container'
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>Not found :(</h1>
-    <p>Sorry, the link you followed either doesn't exist or was removed.</p>
-    <p><Link to="/">Return to home</Link></p>
+    <Container>
+      <section>
+        <h1>Not found :(</h1>
+        <p>Sorry, the link you followed either doesn't exist or was removed.</p>
+        <p><Link to="/">Return to home</Link></p>
+      </section>
+    </Container>
   </Layout>
 )
 
