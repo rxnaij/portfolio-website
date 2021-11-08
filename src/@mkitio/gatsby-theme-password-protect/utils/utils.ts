720cf45c78316dd58a0af21cc040ecacc3c2c844
rxnaij <richardblue37@gmail.com>
2021-11-08 18:20:21 -0500

    try to fix window undefined bug. im trying my best

--- src/@mkitio/gatsby-theme-password-protect/utils/utils.ts
+++ src/@mkitio/gatsby-theme-password-protect/utils/utils.ts
@@ -1,14 +1,18 @@
 import * as utils from '@mkitio/gatsby-theme-password-protect/src/utils/utils'
 
-const storage = typeof window !== "undefined" && window.sessionStorage
+const storage = typeof window !== "undefined" ? window.sessionStorage : null
 const STORAGE_NAME = `gatsby-theme-password-protect`
 
 export const setSessionPassword = (passwordCandidate: string) => {
-    storage?.setItem(STORAGE_NAME, passwordCandidate)
+    if (storage) {
+        storage.setItem(STORAGE_NAME, passwordCandidate)
+    }
 }
 
 export const getSessionPassword = () => {
-    return storage?.getItem(STORAGE_NAME)
+    if (storage) {
+        return storage.getItem(STORAGE_NAME)
+    }
 }
 
 export const {
