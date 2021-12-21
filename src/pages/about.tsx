613286296224f30b76dd8a5d6f893b8b79792c8d
rxnaij <richardblue37@gmail.com>
2021-12-21 15:08:35 -0500

    create about and contact pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Layout from '../components/layout/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout>
+            <section>
+                <h1>About me</h1>
+                <p>
+                    Hey, I'm Richard!
+                </p>
+                <p>
+                    I'm a designer, developer, and human being on a quest to create more compassionate technology.
+                </p>
+                <p>
+                    Growing up, I aspired to become a writer. However, I always had an outside interest in computers and video games, and all the magic they provided, from the Wii. But I decided that CSS was way too complicated.
+                </p>
+                <p>
+                    I also grew up in the emergent days of social media as we know it today, with Facebook and YouTube—supremely powerful tools for connection, but also exacerbants of the pain of growing up as a teenager.
+                </p>
+                <p>
+                    I rediscovered coding in college when I designed websites for a school club. When I discovered UX design in college, I was drawn to the call to design with purpose: that technology creators can directly influence the lives of the people they design for.
+                </p>
+                <p>
+                    Today, I design in the service of creating that magic, and to do so in a responsible and empowering way. Since college, I've taught myself how to develop sites and apps in React.
+                </p>
+                <p>
+                    I'm seeking to work with talented senior designers and developers to build elegant, delightful products that help people solve real problems through incisive design. I'm particularly excited about learning how to design in real company contexts, within real world constraints and communicative contexts.
+                </p>
+            </section>
+            <section>
+                <h2>Find me elsewhere:</h2>
+                <ul>
+                    <li>GitHub</li>
+                    <li>Behance</li>
+                    <li>Instagram</li>
+                </ul>
+                <h2>Interests</h2>
+                <h3>Video games</h3>
+                <p>
+                    they are my lifeblood. 
+                </p>
+                <ul>
+                    <li>Currently aiming to finish Dark Souls 3.</li>
+                    <li>On deck: Metroid Dread, Dragon Quest XI.</li>
+                    <li>I have a gaming blog, check it out here.</li>
+                </ul>
+                <h3>Music</h3>
+                <p>
+                    playing the guitar! Here's a small beat I worked on.
+                </p>
+                <h3>Running</h3>
+                <p>
+                    it's hard work, but I love it!
+                </p>
+            </section>
+        </Layout>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/contact.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import Layout from '../components/layout/layout'
+import Button from '../components/button/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from '../components/form/Form'
+
+const ContactPage = () => {
+    const [ state, setState ] = useState({})
+
+    // Syncs form input values with state (is this necessary?)
+    const handleChange = e => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+
+    return(
+        <Layout>
+            <section id="contact">
+                <h1>Let's talk!</h1>
+                <p>Thanks for stopping by! Leave a message if you're interested in working together, have questions, or want to share ideas.</p>
+                <p>At the moment, <strong className="">I'm currently open to offers for full-time and freelance work.</strong> Woohoo!</p>
+                <Form 
+                    name="contact"
+                    method="post"
+                >
+                    <Form.Group>
+                        <Form.Label htmlFor="name">Your name</Form.Label>
+                        <Form.Input type="text" name="name" required />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label htmlFor="email">Email address</Form.Label>
+                        <Form.Input type="email" name="email" required />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label htmlFor="message">Your message</Form.Label>
+                        <Form.Textarea name="message" rows={4} required />
+                    </Form.Group>
+                    <Button className="d-block ml-auto" variant="primary" type="submit">
+                        Send it!
+                    </Button>
+                </Form>
+            </section>
+        </Layout>
+    )
+}
+
+export default ContactPage
