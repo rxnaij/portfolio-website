3a8f8c7388b3a99f87e473093fa26752351557f0
rxnaij <richardblue37@gmail.com>
2022-01-01 01:34:07 -0500

    add more about styles

---
+++ src/content/about-me.md
@@ -0,0 +1,15 @@
+---
+
+title: "About Me"
+
+---
+
+Hey, I'm Richard! Thanks for stopping by. I'm a designer, developer, and human being who loves creating technology that delights, inspires, and and spreads compassion.
+
+I'm currently seeking a **full-time role** where I can help talented design and engineering teams build solutions to real human needs. I'm particularly excited about using my skills to bridge gaps between design and code.
+
+As a Web 2.0 kid, I loved tinkering around on the web, from drag-and-drop site builders to wikis. But years of boring and gatekeepy computer science classes discouraged me from pursuing that interest further.
+
+Then in college, I was tasked with creating a club website, and oh boy, did that rekindle my love for the web. Not only did I love learning things like HTML/CSS and design trends, I was fascinated to discover that "design" meant much more than fancy aesthetics; it was a deliberate approach to solving real-world problems.
+
+Since then, I created my own Digital Design and User Experience major in college, taught myself web development, and built a number of sites. Today, I believe my role as a technologist is to recognize the responsibility that tech has on others’ lives, and to harness it for human good.
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -14,13 +14,13 @@ const AboutPage = ({ data }) => {
         <Layout>
             <SEO title="About me" />
             <section className="longform-text main-and-sidebar-container">
-                <h1 className="main">About me</h1>
+                <h1 className="main page-title">About me</h1>
                 <div className="main">
                     <div dangerouslySetInnerHTML={{ __html: html }} />
                 </div>
                 <aside className="sidebar">
                     <Stack asList={true} flexDirection="column" gap="sm" className="unstyled-list">
-                        <li>
+                        {/* <li>
                             <Button 
                                 icon={Github}
                                 variant="outline"
@@ -50,12 +50,15 @@ const AboutPage = ({ data }) => {
                             >
                                 Super Save Slot (my gaming blog!)
                             </Button>
-                        </li>
+                        </li> */}
+                        <li><Github size={24} style={{ display: 'inline-block', marginRight: 8 }} /><a href="">GitHub</a></li>
+                        <li><Behance size={24} style={{ display: 'inline-block', marginRight: 8 }} /><a href="">Behance</a></li>
+                        <li><Controller size={24} style={{ display: 'inline-block', marginRight: 8 }} /><a href="https://kind-lumiere-cbb8a1.netlify.app/">Super Save Slot (my gaming blog!)</a></li>
                     </Stack>
                     <hr className="divider" />
-                    <Stack gap="md" flexDirection='column'>
-                        <div>
-                            <h3>Games I'm playing</h3>
+                    <Stack gap="base" flexDirection='column'>
+                        <div className="sidebar-list">
+                            <h3>Games</h3>
                             <h4><Controller size={24} style={{ display: 'inline-block' }}/> Now playing</h4>
                             <ul className="unstyled-list">
                                 <li>Dark Souls III</li>
@@ -63,7 +66,7 @@ const AboutPage = ({ data }) => {
                                 <li>God of War (2018)</li>
                             </ul>
                         </div>
-                        <div>
+                        <div className="sidebar-list">
                             <h4><CloudMoonFill size={24} style={{ display: 'inline-block' }}/> On deck</h4>
                             <ul className="unstyled-list">
                                 <li>Metroid Dread</li>
@@ -71,8 +74,8 @@ const AboutPage = ({ data }) => {
                                 <li>Persona 5 Strikers</li>
                             </ul>
                         </div>
-                        <div>
-                            <h3>Music</h3>
+                        <div className="sidebar-list">
+                            <h4><FileMusicFill size={24} style={{ display: 'inline-block' }} /> Music</h4>
                             <p>
                                 In <strong>love</strong> with <a href="https://open.spotify.com/album/4pJT0WKggr4xk149X8A6KC?si=_8sWFb-4Tqm4iajYh3Yu2g">Persona 5's soundtrack</a>, and trying to learn its songs on guitar!
                             </p>
@@ -88,7 +91,7 @@ export default AboutPage
 
 export const query = graphql`
     query AboutMe {
-        markdownRemark( id: { eq: "0c5efa91-d0ea-55e6-a4f9-7d5d8ad7f25b" } ) {
+        markdownRemark {
             frontmatter {
                 title
             }
