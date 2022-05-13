44feb841f15ace2c4e149ec2ff54a5b08809a33b
Richard Lu <richardblue37@gmail.com>
2022-05-13 01:32:57 -0400

    change appearance of RelevantLink links

--- src/components/index/RelevantLinks.tsx
+++ src/components/index/RelevantLinks.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import { Behance, Github, Envelope, JournalText } from 'react-bootstrap-icons'
-import { wrapper, content } from './RelevantLinks.module.scss'
+import { wrapper, content, linkWrapper, iconWrapper } from './RelevantLinks.module.scss'
 
 const RelevantLinks = () => {
     const data = useStaticQuery(graphql`
@@ -18,12 +18,15 @@ const RelevantLinks = () => {
         <section id="relevant-links" className={wrapper}>
             <h3>Relevant links</h3>
             <ul className={content}>
-                <li>
+                {/* <li>
                     <a href="https://github.com/rxnaij">
                         <Github size={32} /> GitHub
                     </a>
-                </li>
-                <li>
+                </li> */}
+                <Link href="https://github.com/rxnaij" icon={Github} label="GitHub" />
+                <Link href="https://be.net/richardbludesign" icon={Behance} label="Behance" />
+                <Link href="data.contentfulAsset.file.url" icon={JournalText} label="Resume" />
+                {/* <li>
                     <a href="https://be.net/richardbludesign">
                         <Behance size={32}/> Behance
                     </a>
@@ -32,9 +35,23 @@ const RelevantLinks = () => {
                     <a href={data.contentfulAsset.file.url}>
                         <JournalText size={32} /> Resume
                     </a>
-                </li>
+                </li> */}
             </ul>
         </section>
+    )
+}
+
+const Link = ({ href, icon, label }) => {
+    const Icon = icon
+    return(
+        <li>
+            <a className={linkWrapper} href={href}>
+                <div className={iconWrapper}>
+                    <Icon size={32} />
+                </div>
+                <span>{label}</span>
+            </a>
+        </li>
     )
 }
 
